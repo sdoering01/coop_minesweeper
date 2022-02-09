2d20ba3b3019b419d9843b06d2de9342dbaadeb2
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2022-02-09 13:17:26 +0100

    Refactor games to use GenServer instead of Agent

--- lib/coop_minesweeper/game/game.ex
+++ lib/coop_minesweeper/game/game.ex
@@ -1,15 +1,20 @@
 defmodule CoopMinesweeper.Game.Game do
   @moduledoc """
   This module holds the logic to interact with a minesweeper field. All calls
-  to one field are done through its corresponding agent process so that no race
+  to one field are done through its corresponding process so that no race
   conditions occur, when multiple players interact with the same field.
   """
 
-  use Agent, restart: :temporary
+  use GenServer, restart: :temporary
 
   alias CoopMinesweeper.Game.Field
 
-  @spec start_link(opts :: keyword()) :: Agent.on_start() | Field.on_new_error()
+  ## Client API
+
+  @doc """
+  Starts a new game.
+  """
+  @spec start_link(opts :: keyword()) :: GenServer.on_start() | Field.on_new_error()
   def start_link(opts) do
     size = Keyword.fetch!(opts, :size)
     mines = Keyword.fetch!(opts, :mines)
@@ -17,68 +22,77 @@ defmodule CoopMinesweeper.Game.Game do
     visibility = Keyword.fetch!(opts, :visibility)
 
     with {:ok, field} <- Field.new(size, mines, game_id, visibility) do
-      Agent.start_link(fn -> field end, opts)
+      GenServer.start_link(__MODULE__, field, opts)
     end
   end
 
   @doc """
-  Returns the underlying field of the game agent.
+  Returns the underlying field of the game.
   """
-  @spec get_field(game_agent :: pid()) :: Field.t()
-  def get_field(game_agent) do
-    Agent.get(game_agent, & &1)
+  @spec get_field(game :: pid()) :: Field.t()
+  def get_field(game) do
+    GenServer.call(game, :get_field)
   end
 
   @doc """
   Makes a turn in the game.
   """
-  @spec make_turn(game_agent :: pid(), pos :: Field.position(), player :: String.t()) ::
+  @spec make_turn(game :: pid(), pos :: Field.position(), player :: String.t()) ::
           Field.on_make_turn()
-  def make_turn(game_agent, pos, player) do
-    Agent.get_and_update(
-      game_agent,
-      fn field ->
-        case Field.make_turn(field, pos, player) do
-          {:ok, {updated_field, _changes}} = ret -> {ret, updated_field}
-          {:error, _} = err -> {err, field}
-        end
-      end,
-      :timer.seconds(120)
-    )
+  def make_turn(game, pos, player) do
+    GenServer.call(game, {:make_turn, pos, player})
   end
 
   @doc """
   Toggles a mark in the game.
   """
-  @spec toggle_mark(game_agent :: pid(), pos :: Field.position(), player :: String.t()) ::
+  @spec toggle_mark(game :: pid(), pos :: Field.position(), player :: String.t()) ::
           Field.on_toggle_mark()
-  def toggle_mark(game_agent, pos, player) do
-    Agent.get_and_update(
-      game_agent,
-      fn field ->
-        case Field.toggle_mark(field, pos, player) do
-          {:ok, {updated_field, _changes}} = ret -> {ret, updated_field}
-          {:error, _} = err -> {err, field}
-        end
-      end,
-      :timer.seconds(120)
-    )
+  def toggle_mark(game, pos, player) do
+    GenServer.call(game, {:toggle_mark, pos, player})
   end
 
   @doc """
   Restarts a game that is over.
   """
-  @spec play_again(game_agent :: pid()) :: Field.on_play_again()
-  def play_again(game_agent) do
-    Agent.get_and_update(
-      game_agent,
-      fn field ->
-        case Field.play_again(field) do
-          {:ok, updated_field} = ret -> {ret, updated_field}
-          {:error, _} = err -> {err, field}
-        end
-      end,
-      :timer.seconds(120)
-    )
+  @spec play_again(game :: pid()) :: Field.on_play_again()
+  def play_again(game) do
+    GenServer.call(game, :play_again)
+  end
+
+  ## Server API
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call(:get_field, _from, field) do
+    {:reply, field, field}
+  end
+
+  @impl true
+  def handle_call({:make_turn, pos, player}, _from, field) do
+    case Field.make_turn(field, pos, player) do
+      {:ok, {updated_field, _changes}} = ret -> {:reply, ret, updated_field}
+      {:error, _} = err -> {:reply, err, field}
+    end
+  end
+
+  @impl true
+  def handle_call({:toggle_mark, pos, player}, _from, field) do
+    case Field.toggle_mark(field, pos, player) do
+      {:ok, {updated_field, _changes}} = ret -> {:reply, ret, updated_field}
+      {:error, _} = err -> {:reply, err, field}
+    end
+  end
+
+  @impl true
+  def handle_call(:play_again, _from, field) do
+    case Field.play_again(field) do
+      {:ok, updated_field} = ret -> {:reply, ret, updated_field}
+      {:error, _} = err -> {:reply, err, field}
+    end
   end
 end
